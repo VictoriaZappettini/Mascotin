16cabaecfd79812d8f5f7b4ae9526236b2c91e38
vzappettini <vzappettini@sugit.com.ar>
2022-03-25 10:53:18 -0300

    segundo desafio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">M4SC0T1N</a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Alimentos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Comfort</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Medicamentos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Juguetes</a>
+            </li>
+          </ul>
+        </nav>
+        <p style={{marginTop: '350px'}}>
+          Bienvenidos!!! Esta página se esta construyendo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
