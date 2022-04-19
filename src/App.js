37b84b093ed1ebfac355afe543dc8f438b1ce2ea
vzappettini <vzappettini@sugit.com.ar>
2022-04-18 22:11:30 -0300

    Zappettini + mascotin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Home from './pages/Home'
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-       <Home />
+        <BrowserRouter>
+            <NavBar />
+
+            <Routes>
+                <Route path="/" element={<ItemListContainer />} />
+                <Route path="/category/:idCategory" element={<ItemListContainer />} />
+                <Route path="/item/:idItem" element={<ItemDetailContainer />} />
+            </Routes>
+
+        </BrowserRouter>
     </div>
   );
 }
